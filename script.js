6e571f874113177a7182f1a21a4217f62d743465
MonteThmas <76977609+MonteThmas@users.noreply.github.com>
2021-11-30 05:11:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+	//Typing animation script
+var typed = new Typed(".typing",{
+	strings: ["Entry Level Programmer", "Entry Level Analysis", "Web Designer"],
+	typeSpeed: 100,
+	backSpeed: 60,
+	loop: true
+
+});
